5efcacb9e7e5c737ad9649900110dc684f8ec0fc
Scott Pledger <scottpledger2005@gmail.com>
2014-12-15 13:27:43 -0700

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from setuptools import setup, find_packages
+
+setup(
+	name = "LaVa",
+	version = "0.1",
+	packages = find_packages(),
+	scripts = ['analyser.py', 'visualizer.py'],
+	install_requires = ['nltk>=3.0.0'],
+	package_data = {
+		'': ['*.txt']
+	},
+	author = "Scott R. Pledger",
+	author_email = "pledger@colorado.edu",
+	description = "Tool to visualize how authors use language.",
+	license = "GPL",
+	url = "https://github.com/scottpledger/lava"
+)
